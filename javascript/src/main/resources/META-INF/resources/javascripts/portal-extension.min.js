03c032f45dfd07bf24378c850d9cb401bea75086
Donghwan Kim <flowersinthesand@gmail.com>
2013-01-28 16:02:45 +0900

    Update copyright year

--- javascript/src/main/resources/META-INF/resources/javascripts/portal-extension.min.js
+++ javascript/src/main/resources/META-INF/resources/javascripts/portal-extension.min.js
@@ -2,7 +2,7 @@
  * Portal extension
  * http://github.com/flowersinthesand/portal-java
  * 
- * Copyright 2012, Donghwan Kim 
+ * Copyright 2012-2013, Donghwan Kim 
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  */
